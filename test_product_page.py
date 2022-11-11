383178258acb06dbac9b4d5e8fc5ab98ca3a980e
Anastasiya <mudryi-kaa@yandex.ru>
2022-11-11 15:56:02 +0300

    lesson 4.3 path 1 (1) combing the code

--- test_product_page.py
+++ test_product_page.py
@@ -5,13 +5,15 @@ import pytest
 from .pages.product_page import ProductPage
 
 
-# def test_guest_can_add_product_to_basket(browser):
-#     link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_add_to_basket_form()
-#     page.add_to_basket()
-#     page.solve_quiz_and_get_code()
+def test_guest_can_add_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket_form()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_add_the_right_product()
+    page.should_be_right_cost_of_product()
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -24,8 +26,6 @@ from .pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
-    #link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209'
-    #link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_form()
