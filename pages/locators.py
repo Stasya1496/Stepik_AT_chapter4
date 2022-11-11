38b72201d97e99fbac5afb1909fa1fd750a94818
Anastasiya <mudryi-kaa@yandex.ru>
2022-11-11 15:54:25 +0300

    lesson 4.3 path 1 test many product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_FORM = (By.CSS_SELECTOR, "#add_to_basket_form")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_COST = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_ADD_TO_BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages :nth-child(1) > .alertinner strong")
+    MESSAGE_COST_OF_BASKET = (By.CSS_SELECTOR, '#messages :nth-child(3) > .alertinner p strong')
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket_form = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_FORM)
+        add_to_basket_form.click()
+
+    def should_be_add_to_basket_form(self):
+        # Поверка, что есть кнопка добавления в корзину
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET_FORM), "Add to basket form is not presented"
+
+    def should_be_add_the_right_product(self):
+        # Поверка, что в сообщении о добавлении продукта указан нужный продукт
+        product_name = self.browser.find_element(*ProductPageLocators.MESSAGE_ADD_TO_BASKET_PRODUCT_NAME).text
+        add_product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert product_name == add_product_name, "Add to basket product isn't right"
+
+    def should_be_right_cost_of_product(self):
+        # Поверка, что в сообщении о добавлении продукта указан нужный продукт
+        assert self.browser.find_element(
+            *ProductPageLocators.MESSAGE_COST_OF_BASKET).text == self.browser.find_element(
+            *ProductPageLocators.PRODUCT_COST).text, "Add to basket cost isn't right"
